494fdb6b647034f7f03a1288165c398c705b18da
xybai0103 <xueyin0103@gmail.com>
2023-06-14 12:14:51 -0500

    change return result to return result.value for getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -33,7 +33,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 initdb();
